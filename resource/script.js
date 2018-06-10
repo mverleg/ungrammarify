23b3218205d531ccc80d8e49353a02b792474542
Mark <mverleg.noreply@gmail.com>
2018-06-10 15:03:34 +0200

    Got editors to show up at least

--- resource/script.js
+++ resource/script.js
@@ -2,28 +2,46 @@
 'use strict';
 
 (function () {
-    // require("ace/commands/default_commands").commands.push({
-    //     name: "Toggle Fullscreen",
-    //     bindKey: "F11",
-    //     exec: function(editor) {
-    //         dom.toggleCssClass(document.body, "fullScreen");
-    //         dom.toggleCssClass(editor.container, "fullScreen");
-    //         editor.resize();
-    //     }
-    // });
 
-    let editor = ace.edit("editor", {
-        mode: "ace/mode/javascript",
-        selectionStyle: "text"
+    var editor, output;
+
+    require(["ace/ace"], function (ace) {
+        /* Input */
+        editor = window.ace.edit("editor", {
+            mode: "ace/mode/text",
+            selectionStyle: "text"
+        });
+
+        editor.setOptions({
+            autoScrollEditorIntoView: true,
+            copyWithEmptySelection: true,
+            highlightActiveLine: true,
+            showPrintMargin: false,
+            theme: "ace/theme/twilight"
+        });
+        editor.session.setUseWrapMode(true);
+
+        /* Output */
+        output = window.ace.edit("output", {
+            mode: "ace/mode/text",
+            selectionStyle: "text"
+        });
+
+        output.setOptions({
+            autoScrollEditorIntoView: true,
+            copyWithEmptySelection: true,
+            highlightActiveLine: true,
+            showPrintMargin: false,
+            theme: "ace/theme/twilight"
+        });
+        output.session.setUseWrapMode(true);
+        output.setReadOnly(true);
     });
 
-    editor.setOptions({
-        autoScrollEditorIntoView: true,
-        copyWithEmptySelection: true,
-    });
-    editor.session.setUseWrapMode(true);
-    editor.setHighlightActiveLine(false);
+    document.getElementById("ungrammarify_btn").addEventListener("click", function(e) {
+        output.setValue(editor.getValue());
+        // console.warn("clicked", editor.getValue(), output);
+    }, false);
 
-    editor.setTheme("ace/theme/twilight");
 })();
 
