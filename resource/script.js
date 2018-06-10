64c22b7cad3015df08cda4c8cfc3e38a575b460e
Mark <mverleg.noreply@gmail.com>
2018-06-10 15:59:26 +0200

    More stuff like button and warnings, but no kotlin yet

--- resource/script.js
+++ resource/script.js
@@ -3,7 +3,26 @@
 
 (function () {
 
-    var editor, output;
+    let editor = null, output = null;
+
+    function show_user_warning(msg) {
+        console.error(msg);
+        let popup = document.createElement('p');
+        popup.classList.add('user_warning_popup');
+        popup.appendChild(document.createTextNode(msg));
+        document.body.appendChild(popup);
+        setTimeout(function() {
+            document.body.removeChild(popup);
+        }, 4000);
+    }
+
+    function ungrammarify_action() {
+        if (editor === null || output === null) {
+            show_user_warning("Cannot ungrammarify now, the editor has not been loaded (yet).");
+            return;
+        }
+        output.setValue(editor.getValue());
+    }
 
     require(["ace/ace"], function (ace) {
         /* Input */
@@ -36,11 +55,14 @@
         });
         output.session.setUseWrapMode(true);
         output.setReadOnly(true);
+
+        /* Process the initial text */
+        ungrammarify_action();
     });
 
     document.getElementById("ungrammarify_btn").addEventListener("click", function(e) {
-        output.setValue(editor.getValue());
-        // console.warn("clicked", editor.getValue(), output);
+        // This needs the Ace editors to have been initialized.
+        ungrammarify_action();
     }, false);
 
 })();
